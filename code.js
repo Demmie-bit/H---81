9dae5ad2621411505e10570bcaac6e5daf9ca172
Demmie-bit <77945051+Demmie-bit@users.noreply.github.com>
2021-08-02 11:56:34 -0500

    Add files via upload

---
+++ code.js
@@ -0,0 +1,58 @@
+canvas = document.getElementById("myCanvas");
+ctx= canvas.getContext("2d");
+
+color = "red"; 
+
+ctx.beginPath();
+ctx.strokeStyle = "blue";
+ctx.lineWidth = 5;
+ctx.arc(200, 200, 40 ,0 , 2*Math.PI);
+ctx.stroke();
+ctx.beginPath();
+ctx.strokeStyle = "black";
+ctx.lineWidth = 5;
+ctx.arc(290, 200, 40 ,0 , 2*Math.PI);
+ctx.stroke();
+ctx.beginPath();
+ctx.strokeStyle = "red";
+ctx.lineWidth = 5;
+ctx.arc(380, 200, 40 ,0 , 2*Math.PI);
+ctx.stroke();
+ctx.beginPath();
+ctx.strokeStyle = "green";
+ctx.lineWidth = 5;
+ctx.arc(335, 250, 40 ,0 , 2*Math.PI);
+ctx.stroke();
+ctx.beginPath();
+ctx.strokeStyle = "yellow";
+ctx.lineWidth = 5;
+ctx.arc(240, 250, 40 ,0 , 2*Math.PI);
+ctx.stroke();
+
+canvas.addEventListener("mousedown", my_mousedown);
+
+function my_mousedown(e){
+    color = document.getElementById("color").value;
+    console.log(color);
+     mouse_x = e.clientX - canvas.offsetLeft;
+     mouse_y = e.clientY - canvas.offsetTop;
+    console.log("X = " + mouse_x + " ,Y =  " + mouse_y);
+    circle(mouse_x , mouse_y);    
+}
+
+function circle(mouse_x , mouse_y){
+ctx.beginPath();
+ctx.strokeStyle = "black";
+ctx.lineWidth = 2;
+ctx.arc(mouse_x, mouse_y, 40 ,0 , 2*Math.PI);
+ctx.strokeStyle = color;
+ctx.lineWidth = 2;
+ctx.arc(mouse_x, mouse_y, 40 ,0 , 2*Math.PI);
+ctx.stroke();
+}
+
+function clearArea(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+	
